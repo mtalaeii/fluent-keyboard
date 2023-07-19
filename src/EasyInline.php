f3dc27df0ccec1fbde6d9476f1a3cdda22ed15d3
َAhJ <AmirHosseinJafari8228@gmail.com>
2023-07-19 16:45:41 +0330

    Add All Short Inline Methods

--- src/EasyInline.php
+++ src/EasyInline.php
@@ -2,14 +2,37 @@
 
 namespace EasyKeyboard\FluentKeyboard;
 
-trait EasyMethod
+use EasyKeyboard\FluentKeyboard\ButtonTypes\InlineButton;
+
+trait EasyInline
 {
-    public static function addCallback(string $text, string $callback)
+    public function addCallback(string $text, string $callback)
     {
-        $data = [
-            '_' => 'keyboardButtonCallback',
-            'text' => $text,
-            'data' => $callback
-        ];
+        return $this->addButton(InlineButton::CallBack($text, $callback));
+    }
+
+    public function addWebApp(string $text, string $url)
+    {
+        return $this->addButton(InlineButton::WebApp($text, $url));
+    }
+
+    public function addUrl(string $text, string $url)
+    {
+        return $this->addButton(InlineButton::Url($text, $url));
+    }
+
+    public function addGame(string $text)
+    {
+        return $this->addButton(InlineButton::Game($text));
+    }
+
+    public function addBuy(string $text)
+    {
+        return $this->addButton(InlineButton::Buy($text));
+    }
+
+    public function addSwitchInline(string $text, string $query, bool $same_peer = true)
+    {
+        return $this->addButton(InlineButton::SwitchInline($text, $query, $same_peer));
     }
 }
